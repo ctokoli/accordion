fa9ddeafdff46edae7b274eb1333eac058773ae7
Cephas Tokoli <matumasie@gmail.com>
2024-03-01 10:34:20 +0000

    Add data.tsx

---
+++ src/components/data.tsx
@@ -0,0 +1,38 @@
+export type Data = {
+  id: number,
+  question: string,
+  answer: string
+}
+
+const dataArray: Data[] = [
+    {
+      "id": 1,
+      "question": "What is the capital of France?",
+      "answer": "Paris"
+    },
+    {
+      "id": 2,
+      "question": "Who wrote 'Romeo and Juliet'?",
+      "answer": "William Shakespeare"
+    },
+    {
+      "id": 3,
+      "question": "What is the boiling point of water in Celsius?",
+      "answer": "100 degrees"
+    },
+    {
+      "id": 4,
+      "question": "What is the largest planet in our solar system?",
+      "answer": "Jupiter"
+    },
+    {
+      "id": 5,
+      "question": "What year did the Titanic sink?",
+      "answer": "1912"
+    }
+  ]
+
+  export default dataArray;
+
+  
+  
